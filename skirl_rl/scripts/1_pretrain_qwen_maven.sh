dabd987e9f0074ddbeb72f8aa37fb855e02f0f5f
root <wandugu.com>
2025-10-26 02:12:52 +0800

    sft wandb

--- skirl_rl/scripts/1_pretrain_qwen_maven.sh
+++ skirl_rl/scripts/1_pretrain_qwen_maven.sh
@@ -4,15 +4,15 @@ set -euo pipefail
 ROOT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")/../.." && pwd)
 CONFIG_PATH="${ROOT_DIR}/configs/pretrain_maven.yaml"
 
-echo "$CONFIG_PATH"; test -f "$CONFIG_PATH" && head -n 5 "$CONFIG_PATH"
+echo "$CONFIG_PATH"; test -f "$CONFIG_PATH"
 
 
 cd "${ROOT_DIR}"
 
-if [ ! -f "data/processed/maven_sft.jsonl" ]; then
-  echo "[INFO] 数据未找到，自动生成演示样本。"
-  python skirl_rl/scripts/0_convert_maven_to_event_traj.py
-fi
+# if [ ! -f "data/processed/maven_sft_train.jsonl" ]; then
+  # echo "[INFO] 数据未找到，自动生成演示样本。"
+  # python skirl_rl/scripts/0_convert_maven_to_event_traj.py
+# fi
 
 if [ ! -f "requirements.txt" ]; then
   echo "[ERROR] requirements.txt 不存在"
@@ -22,6 +22,6 @@ fi
 # python -m pip install --upgrade pip >/dev/null
 # python -m pip install -r requirements.txt
 
-echo "[INFO] 启动 LlamaFactory 预训练"
+echo "[INFO] 启动 maven sft"
 llamafactory-cli train "${CONFIG_PATH}"
 
